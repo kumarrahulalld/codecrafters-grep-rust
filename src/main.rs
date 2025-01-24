cca57ad4dc58257c372ac3d854b1eafff341deeb
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2025-01-24 22:39:38 +0530

    debug

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ fn match_pattern(input_line: &str, pattern: &str, ind: usize, pind: usize) -> bo
     // Log the current indices and characters being compared
     println!("Matching input[{}]: '{}' with pattern[{}]: '{}'", ind, input_line.chars().nth(ind).unwrap_or(' '), pind, pattern.chars().nth(pind).unwrap_or(' '));
 
-    // If we have reached the end of the pattern
-    if pind >= pattern.len() || ind >= input_line.len() {
-        return false;
-    }
+    // If we have reached the end of the patters
 
     if pind == pattern.len() {
         println!("Base case reached, pattern matched.");
