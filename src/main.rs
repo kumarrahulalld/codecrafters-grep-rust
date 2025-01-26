ac6b603701b976991f61d380283eaff2b88f7500
RahulKumarMishra <kumarrahul.allduniv@gmail.com>
2025-01-26 23:23:59 +0530

    debug

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,6 @@ fn match_pattern(input_line: &str, pattern: &str, ind: usize, pind: usize) -> bo
     if pind >= pattern.chars().count() {
         return true;
     }
-    if  ind >= input_line.chars().count() {
-        return pind==pattern.chars().count();
-    }
     //println!("{} {}",ind,input_line.chars().count());
     // Log the current indices and characters being compared
     println!("Matching input[{}]: '{}' with pattern[{}]: '{}'", ind, input_line.chars().nth(ind).unwrap_or(' '), pind, pattern.chars().nth(pind).unwrap_or(' '));
